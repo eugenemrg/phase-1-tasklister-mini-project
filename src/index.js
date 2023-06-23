80eb10c93e7e21726361ca162a398c8243bccec7
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-06-23 08:36:11 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const input = document.getElementById('new-task-description');
+  const taskList = document.getElementById('tasks')
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    let taskInput = input.value
+
+    let taskListItem = document.createElement('li')
+    taskListItem.textContent = `${taskInput}  `
+
+    let button = document.createElement('button')
+    button.textContent = 'x'
+    button.addEventListener('click', (e) => {
+      e.target.parentNode.remove()
+    })
+
+    taskListItem.append(button)
+
+    taskList.append(taskListItem)
+  });
+
 });
